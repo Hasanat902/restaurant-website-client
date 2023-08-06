9c8b54314fc9478728cc91d5f85b9c21f78cddab
Hasanat902 <abulhasanat902@gmail.com>
2023-08-06 20:33:01 +0600

    added testimonials in homepage

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,13 +4,13 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle
                 subHeading={"Check it Out"}
                 heading={"Featured Item"}
             ></SectionTitle>
 
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -18,7 +18,7 @@ const Featured = () => {
                     <p>Aug 10, 2026</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odit impedit, harum aperiam magni inventore commodi deserunt perferendis sit accusamus dolores ratione debitis, dicta vitae possimus earum obcaecati libero ipsa natus, enim maxime deleniti molestias! Omnis reprehenderit recusandae earum atque consequatur, veniam excepturi saepe vitae nemo facere, dolor adipisci dolore?</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
