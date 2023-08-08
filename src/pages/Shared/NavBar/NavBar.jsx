87a32a51250ba77abd956788733070c56def8010
Hasanat902 <abulhasanat902@gmail.com>
2023-08-08 18:09:40 +0600

    implement private route

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -16,9 +16,11 @@ const NavBar = () => {
       <li><Link to="/">Home</Link></li>
       <li><Link to="/menu">Our Menu</Link></li>
       <li><Link to="/order/salad">Order Food</Link></li>
+      <li><Link to="/secret">Secret</Link></li>
       
       {
         user ? <>
+          <span>{user?.displayName}</span>
           <button onClick={handleLogOut} className="btn btn-ghost btn-xs mt-2">LogOut</button>
         </> : 
         <>
