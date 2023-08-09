331177790771a283489a2eb85aec18d05b98cc7b
Hasanat902 <abulhasanat902@gmail.com>
2023-08-09 14:41:56 +0600

    implement add to cart functionality and show it on navbar

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,6 +1,56 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import useCart from "../../hooks/useCart";
 
 const FoodCard = ({item}) => {
-    const {name, price, image, recipe} = item;
+    const {name, price, image, recipe, _id} = item;
+    const {user} = useContext(AuthContext);
+    const [, refetch] = useCart();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleAddToCart = item => {
+        console.log(item);
+        if(user && user.email){
+          const cartItem = {menuItemId: _id, name, price, image, email: user.email}
+          fetch('http://localhost:5000/carts', {
+            method: "POST",
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify(cartItem)
+          })
+          .then(res => res.json())
+          .then(data => {
+            if(data.insertedId){
+              refetch();
+              Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Food added to the cart',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            }
+          })
+        }
+        else{
+          Swal.fire({
+            title: 'Please login to order the food',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Login Now'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              navigate('/login', {state: {from: location}});
+            }
+          })
+        }
+    }
 
   return (
     <div className="card w-80 bg-base-100 shadow-xl">
@@ -15,7 +65,7 @@ const FoodCard = ({item}) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-outline bg-slate-100 border-0 border-orange-400 border-b-4 mt-4">Add to Cart</button>
+          <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-orange-400 border-b-4 mt-4">Add to Cart</button>
         </div>
       </div>
     </div>
