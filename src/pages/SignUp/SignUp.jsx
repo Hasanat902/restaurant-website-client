ae3ecefcced8ba712a44837099033344db4cae04
Hasanat902 <abulhasanat902@gmail.com>
2023-08-10 13:30:51 +0600

    created google signin

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const SignUp = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
@@ -128,7 +129,8 @@ const SignUp = () => {
                     <input className="btn btn-primary" type="submit" value="Sign Up" />
                 </div>
             </form>
-            <p><small>Already have an account? <Link to='/login'>Please Login</Link></small></p>
+                <p><small>Already have an account? <Link to='/login'>Please Login</Link></small></p>
+                <SocialLogin></SocialLogin>
             </div>
         </div>
         </div>
