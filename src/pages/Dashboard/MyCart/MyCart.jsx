475432801dc97d69339859c43d0bf76b86f48afa
Hasanat902 <abulhasanat902@gmail.com>
2023-08-15 16:22:20 +0600

    implement payment functionality

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
 
 const MyCart = () => {
   const [cart, refetch] = useCart();
@@ -45,7 +46,9 @@ const MyCart = () => {
       <div className="font-semibold h-[60px] flex justify-evenly items-center">
         <h2 className="text-2xl">Total Items: {cart.length}</h2>
         <h2 className="text-2xl">Total Price: ${total}</h2>
-        <button className="btn btn-warning btn-sm">PAY</button>
+        <Link to="/dashboard/payment">
+          <button className="btn btn-warning btn-sm">PAY</button>
+        </Link>
       </div>
       <div className="overflow-x-auto">
         <table className="table">
